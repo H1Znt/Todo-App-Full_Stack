74cdbeee65d992980365f109fe215fa29ede5790
Andrey Maximov <arman.gen40@mail.ru>
2025-08-08 15:49:03 +0300

    Server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import { config } from './config';
 
-const PORT = config.port;
+const PORT = config.port || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
