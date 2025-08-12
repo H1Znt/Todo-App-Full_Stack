088b40a0935cff55c72b6f6b67ead577de13e357
Andrey Maximov <arman.gen40@mail.ru>
2025-08-12 15:06:12 +0300

    React-router, Provider.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import TodosPage from "./pages/TodosPage";
+import { useAppSelector } from "./store/hooks";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { accessToken } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!accessToken && !["/login", "/register"].includes(location.pathname)) {
+      navigate("/login");
+    }
+  }, [accessToken, navigate, location.pathname]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-[#FFE8D6] text-[#6B705C] transition-colors">
+      <div className="container mx-auto px-4 py-12">
+        <Routes>
+          <Route
+            path="/login"
+            element={!accessToken ? <LoginPage /> : <Navigate to="/todos" />}
+          />
+          <Route
+            path="/register"
+            element={!accessToken ? <RegisterPage /> : <Navigate to="/todos" />}
+          />
+          <Route
+            path="/todos"
+            element={accessToken ? <TodosPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="*"
+            element={
+              <Navigate to={accessToken ? "/todos" : "/login"} replace />
+            }
+          />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <ToastContainer
+        position="top-right"
+        theme="colored"
+        autoClose={1800}
+        pauseOnHover={false}
+        pauseOnFocusLoss={false}
+        hideProgressBar
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
